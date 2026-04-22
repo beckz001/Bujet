30632718b6de96d5d68a71ec542cc25bfd4b9a8d
Zachary Beck <zachary.beck@dyson.com>
2026-04-22 12:29:12 +0100

    Index out of range bug was re-introduced on commit d7c01d3. Fix is re-added and bug no longer appears. Re-fixed ngrok link to now be correct.

--- Bujet/BackendConfiguration.swift
+++ Bujet/BackendConfiguration.swift
@@ -12,7 +12,7 @@ enum BackendConfiguration {
         #if targetEnvironment(simulator)
         URL(string: "http://127.0.0.1:3001")!
         #else
-        URL(string: "https://twannaing-unabating-floy.ngrok-free.dev")!
+        URL(string: "https://twanna-unabetting-floy.ngrok-free.dev")!
         #endif
     }
 }
